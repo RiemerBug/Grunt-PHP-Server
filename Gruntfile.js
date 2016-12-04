fa0b5d7c4da23147791434e7422c50bab8dc38df
René Woy <rene@woy.me>
2016-12-04 14:19:58 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+
+    grunt.initConfig({
+        php: {
+
+            dist: {
+                options: {
+                   bin: "C:\\php\\php-7.1.0-Win32-VC14-x64\\php.exe",
+                    port: 50000,
+                    //base: '/',
+                    //keepalive: true,
+                   open: true
+                }
+            }
+        },
+        watch:{
+            files: ['*.php', 'style/*.css', 'js/*.js' ],
+            options: {
+                livereload: true,
+            }
+
+        }
+    });
+
+    grunt.registerTask('default', ['php', 'watch']);
+}
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>GruntPHP</title>
+    <!-- build:css app.css -->
+
+    <!-- /build -->
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="initial-scale=1.0 maximum-scale=1.0">
+    <!-- build:remove -->
+    <script src="//127.0.0.1:35729/livereload.js"></script>
+    <link rel="stylesheet" href="style/main.css">
+
+
+    <!-- /build -->
+</head>
+<body>
+<div id="app-container">hallo world</div>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rwoy
+ * Date: 25.11.2016
+ * Time: 16:51
+ */
+
+
+
+echo "<h1>Test</h1>";
+
+?>
+</body>
+</html>
